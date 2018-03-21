b38b92f5d6c4f3f631dd5391638dddb6c17050b6
mauricionm <mau18nm@gmail.com>
2018-03-20 21:45:28 -0600

    Actulizadas las tablas y pagina de leer codigo QR.

--- joyeriaSYS/LeerQR.aspx.designer.cs
+++ joyeriaSYS/LeerQR.aspx.designer.cs
@@ -47,15 +47,15 @@ namespace joyeriaSYS {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtCodigo;
-        
+
         /// <summary>
-        /// txtNumeroFactura control.
+        /// ddlFactura control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtNumeroFactura;
+        protected global::System.Web.UI.WebControls.DropDownList ddlFactura;
         
         /// <summary>
         /// ddlProducto control.
