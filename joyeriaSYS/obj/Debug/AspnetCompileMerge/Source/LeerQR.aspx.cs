73e3495ac74c90a18d9745eab8231a2cdc247ff1
Bryan Cervantes <bxcndiego@gmail.com>
2018-03-26 15:24:32 -0600

    Ya guarda en el excel con los datos de la factura, faltan los del vendedor y fecha

---
+++ joyeriaSYS/obj/Debug/AspnetCompileMerge/Source/LeerQR.aspx.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using Gma.QrCodeNet.Encoding;
+using System.Drawing;
+using System.IO;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using System.Drawing.Imaging;
+using System.Data;
+using joyeriaSYS.Models;
+using joyeriaSYS.Controles.clases;
+using System.Web.UI;
+using System.Collections.Generic;
+
+namespace joyeriaSYS
+{
+    public partial class LeerQR : Page
+    {
+        private Producto objProd = new Producto();
+        private Clientes objCli = new Clientes();
+        private Factura objFact = new Factura();
+        private DetalleFactura objDeF = new DetalleFactura();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                cargarFacturas();
+            }
+        }
+
+        protected void gvwDetalleFactura_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gvwFacturas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCalcular_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void txtCodigo_TextChanged(object sender, EventArgs e)
+        {
+            var codProducto = (txtCodigo.Text != "") ? Convert.ToInt32(txtCodigo.Text) : 0;
+            var codFactura = (ddlFactura.Text != "") ? Convert.ToInt32(ddlFactura.Text) : 0;
+            if (codFactura != 0 && codFactura != 0)
+            {
+                gestionarCargaDatos(codProducto, codFactura);
+            }
+        }
+
+        protected void ddlFactura_DropDownChanged(object sender, EventArgs e)
+        {
+            var codProducto = (txtCodigo.Text != "") ? Convert.ToInt32(txtCodigo.Text) : 0;
+            var codFactura = (ddlFactura.Text != "") ? Convert.ToInt32(ddlFactura.Text) : 0;
+            if (codFactura != 0 && codFactura != 0)
+            {
+                gestionarCargaDatos(codProducto, codFactura);
+            }
+        }
+
+        private void gestionarCargaDatos(int codProducto, int codFactura)
+        {
+            try
+            {
+                var facturaActual = new FAC_FACTURA();
+                var detFacturaActual = new DEF_DETALLE_FACTURA();
+                var productoActual = new PRO_PRODUCTO();
+                productoActual.CodigoNumerico = codProducto;
+                productoActual = objProd.ConsultarPorCodigoProducto(productoActual);
+                facturaActual.NoFactura = codFactura;
+                facturaActual = objFact.ConsultaPorNumeroDeFactura(facturaActual);
+                detFacturaActual.idFactura = facturaActual.idFactura;
+                detFacturaActual.idProducto = productoActual.IdProducto;
+                detFacturaActual = objDeF.ConsultarPorIdFacturaYIdProducto(detFacturaActual);
+                cargarClientes(facturaActual.idCliente);
+                txtCodTabla.Text = facturaActual.CodTabla;
+
+                cargarProductos(productoActual.IdProducto);
+                CargarTablaDetalleFacturas(facturaActual.idFactura);
+                //poner en detalle un campo para ir descontando los productos escaneados
+
+                cargarTablaFacturas(codFactura);
+            }
+            catch (Exception ex)
+            {
+                var err = ex.Message;
+            }
+        }
+
+        private void cargarTablaFacturas(int codFactura)
+        {
+            try
+            {
+                var facturaActual = new FAC_FACTURA();
+                facturaActual.NoFactura = codFactura;
+                var dt = new DataTable();
+                var row = objFact.ConsultaPorNumeroDeFactura(facturaActual);
+
+                dt.Columns.Add("idFactura", typeof(System.String));
+                dt.Columns.Add("NoFactura", typeof(System.String));
+                dt.Columns.Add("montoFactura", typeof(System.String));
+                dt.Columns.Add("estado", typeof(System.String));
+                dt.Columns.Add("totalPiezas", typeof(System.String));
+                dt.Columns.Add("idCliente", typeof(System.String));
+                //dt.Columns.Add("fechaCreacion", typeof(System.String));
+                //dt.Columns.Add("fechaLiquidacion", typeof(System.String));
+
+                DataRow fila = dt.NewRow();
+
+                fila["idFactura"] = row.idFactura;
+                fila["NoFactura"] = row.NoFactura;
+                fila["montoFactura"] = row.montoFactura;
+                fila["estado"] = row.estado;
+                fila["totalPiezas"] = row.totalPiezas;
+                fila["idCliente"] = row.idCliente;
+                //fila["fechaCreacion"] = r.fechaCreacion;
+                //fila["fechaLiquidacion"] = r.fechaLiquidacion;
+                dt.Rows.Add(fila);
+                gvwFacturas.DataSource = dt;
+                gvwFacturas.DataBind();
+            }
+            catch (Exception ex)
+            {
+                var err = ex.Message;
+            }
+        }
+
+        private void cargarFacturas()
+        {
+            try
+            {
+                var dt = new DataTable();
+                var rows = objFact.Consultar();
+
+                ddlFactura.DataTextField = "NoFactura";
+                ddlFactura.DataValueField = "NoFacturaValue";
+                dt.Columns.Add("NoFactura", typeof(System.String));
+                dt.Columns.Add("NoFacturaValue", typeof(System.String));
+
+                foreach (FAC_FACTURA r in rows)
+                {
+                    DataRow fila = dt.NewRow();
+
+                    fila["NoFactura"] = r.NoFactura;
+                    fila["NoFacturaValue"] = r.NoFactura;
+                    dt.Rows.Add(fila);
+                }
+                ddlFactura.DataSource = dt;
+                ddlFactura.DataBind();
+                ddlFactura.SelectedValue = ddlFactura.ToString();
+            }
+            catch (Exception ex)
+            {
+                var err = ex.Message;
+            }
+        }
+
+        private void cargarClientes(int idCliente)
+        {
+            try
+            {
+                var dt = new DataTable();
+                var rows = objCli.Consultar();
+
+                ddlCliente.DataTextField = "NombreEncargado";
+                ddlCliente.DataValueField = "idCliente";
+                dt.Columns.Add("idCliente", typeof(System.String));
+                dt.Columns.Add("NombreEncargado", typeof(System.String));
+
+                foreach (CLI_CLIENTES r in rows)
+                {
+                    DataRow fila = dt.NewRow();
+
+                    fila["idCliente"] = r.idCliente;
+                    fila["NombreEncargado"] = r.NombreEncargado;
+                    dt.Rows.Add(fila);
+                }
+                ddlCliente.DataSource = dt;
+                ddlCliente.DataBind();
+                ddlCliente.SelectedValue = idCliente.ToString();
+            }
+            catch (Exception ex)
+            {
+                var err = ex.Message;
+            }
+        }
+
+        private void cargarProductos(int idProducto)
+        {
+            try
+            {
+                var dt = new DataTable();
+                var rows = objProd.Consultar();
+
+                ddlProducto.DataTextField = "NombreProducto";
+                ddlProducto.DataValueField = "IdProducto";
+                dt.Columns.Add("IdProducto", typeof(System.String));
+                dt.Columns.Add("NombreProducto", typeof(System.String));
+
+                foreach (PRO_PRODUCTO r in rows)
+                {
+                    DataRow fila = dt.NewRow();
+
+                    fila["IdProducto"] = r.IdProducto;
+                    fila["NombreProducto"] = r.NombreProducto;
+                    dt.Rows.Add(fila);
+                }
+                ddlProducto.DataSource = dt;
+                ddlProducto.DataBind();
+                ddlProducto.SelectedValue = idProducto.ToString();
+            }
+            catch (Exception ex)
+            {
+                var err = ex.Message;
+            }
+        }
+        private void CargarTablaDetalleFacturas(int idFactura)
+        {
+            try
+            {
+                var dt = new DataTable();
+                var rows = objDeF.Consultar();
+                gvwDetalleFactura.DataSource = null;
+                gvwDetalleFactura.DataBind();
+                if (idFactura != -1)
+                {
+                    rows = objDeF.ConsultarPorIdFactura(idFactura);
+                }
+
+                dt.Columns.Add("idFactura", typeof(System.String));
+                dt.Columns.Add("idProducto", typeof(System.String));
+                dt.Columns.Add("codProducto", typeof(System.String));
+                dt.Columns.Add("CantidadProducto", typeof(System.String));
+                //dt.Columns.Add("Precio", typeof(System.String));
+                //dt.Columns.Add("Inventario", typeof(System.String));
+
+                // Recorrer las filas.
+                int posicionConsidencia = -1;
+                int contadorGeneral = 0;
+                foreach (DEF_DETALLE_FACTURA r in rows)
+                {
+                    // Crear una fila por cada unidad del producto.
+                    int cantidad = Convert.ToInt32(r.CantidadProducto);
+                    for (int i = 0; i < cantidad; i++)
+                    {
+                        // Crear la fila, asignar valores y agregarla.
+                        DataRow fila = dt.NewRow();
+                        fila["idFactura"] = r.idFactura;
+                        fila["idProducto"] = r.idProducto;
+
+                        // Revisar si hay considencia con el codigo que se busca.
+                        var productoConsulta = new PRO_PRODUCTO();
+                        productoConsulta.IdProducto = r.idProducto;
+                        var arrayProducto = objProd.ConsultarPorId(productoConsulta);
+                        foreach (PRO_PRODUCTO producto in arrayProducto)
+                        {
+                            // Cargar el codigo del producto en la fila.
+                            fila["codProducto"] = producto.CodigoNumerico;
+                            // Ver si hay considencias con el codigo que se busca.
+                            var codProducto = (txtCodigo.Text != "") ? Convert.ToInt32(txtCodigo.Text) : 0;
+                            if (codProducto == producto.CodigoNumerico)
+                            {
+                                posicionConsidencia = contadorGeneral;
+                            }
+                        }
+                        // La catidad siempre va ser 1.
+                        fila["CantidadProducto"] = "1";
+                        dt.Rows.Add(fila);
+                        contadorGeneral++;
+                    }
+                }
+                gvwDetalleFactura.DataSource = dt;
+                gvwDetalleFactura.DataBind();
+                if (posicionConsidencia != -1)
+                {
+                    gvwDetalleFactura.SelectRow(posicionConsidencia);
+                }
+            }
+            catch (Exception ex)
+            {
+                var err = ex.Message;
+            }
+        }
+    }
+}
---
+++ joyeriaSYS/obj/Debug/AspnetCompileMerge/Source/QR.aspx.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using Gma.QrCodeNet.Encoding;
+using System.Drawing;
+using System.IO;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using System.Drawing.Imaging;
+using System.Data;
+using joyeriaSYS.Controles.clases;
+using joyeriaSYS.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace joyeriaSYS
+{
+    public partial class QR : Page
+    {
+        private Producto objProd = new Producto();
+        private Categoria objCat = new Categoria();
+        private IdQR objQR = new IdQR();
+        QrEncoder encoder = new QrEncoder();
+        QrCode en = new QrCode();
+        public MemoryStream ms = new MemoryStream();
+        public BitMatrix Matrix;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //QRImage.ImageUrl = "../images/pic01.jpg";
+            cargarCategoria();
+            CargarTablaProductos();
+        }
+        public void MostrarMensaje(string Mensaje) {
+            ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('" + Mensaje + "');", true);
+        }
+        public void ErrorMensajer(string Mensaje)
+        {
+            ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('" + Mensaje + "');", true);
+        }
+        public Image Render(MemoryStream ms)
+        {
+            //Image returnImage = Image.FromStream(ms, true);//Exception occurs here
+            //try
+            //{
+                var render = new GraphicsRenderer(new FixedModuleSize(2, QuietZoneModules.Two));
+                render.WriteToStream(Matrix, ImageFormat.Png, ms);
+                //ContentType = "image/png";
+                var returnImage = Image.FromStream(ms, true);//Exception occurs here
+            //}
+            //catch(Exception ex)
+            //{
+                //var err = ex.Message;
+            //}
+            return returnImage;
+        }
+
+        public void cargarCategoria()
+        {
+            try
+            {
+                var dt = new DataTable();
+                var rows = objCat.Consultar();
+
+                ddlCategoria.DataTextField = "Nombre";
+                ddlCategoria.DataValueField = "idCategoria";
+                dt.Columns.Add("idCategoria", typeof(System.String));
+                dt.Columns.Add("Nombre", typeof(System.String));
+
+                foreach (CAT_CATEGORIA r in rows)
+                {
+                    DataRow fila = dt.NewRow();
+
+                    fila["idCategoria"] = r.idCategoria;
+                    fila["Nombre"] = r.Nombre;
+                    dt.Rows.Add(fila);
+                }
+                ddlCategoria.DataSource = dt;
+                ddlCategoria.DataBind();
+            }
+            catch (Exception ex)
+            {
+                var err = ex.Message;
+            }
+        }
+        public string concatenarInformacion()
+        {
+            return txtCodNumerico.Text + ";" + ddlCategoria.SelectedValue;
+        }
+
+        public string generarQR(string idQR)
+        {
+            var codigoParaQR = concatenarInformacion();
+            //var ruta = "C:\\users\\cerva\\documents\\visual studio 2015\\Projects\\joyeriaSYS\\joyeriaSYS\\ImagenesQR\\" + idQR + ".png";
+            var ruta = "C:\\Users\\Administrator\\Documents\\CodigosQR\\" + idQR + ".png";
+            Matrix = encoder.Encode(codigoParaQR).Matrix;
+            ms = new MemoryStream();
+            Image q = Render(ms);
+            q.Save(ruta);
+
+            //QRImage.ImageUrl = "../ImagenesQR/" + txtNombreProducto.Text + txtCodNumerico.Text + ".png";
+            //QRImage.ImageUrl = "../ImagenesQR/" + idQR + ".png";
+            QRImage.ImageUrl = "/CodigosQR/" + idQR + ".png";
+            return ruta;
+        }
+
+        protected void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtCantidad.Text = "";
+            txtCodNumerico.Text = "";
+            //txtNombreProducto.Text = "";
+            txtPrecio.Text = "";
+            hdfId.Value = "-1";
+            //QRImage.ImageUrl = "../images/pic01.jpg";
+        }
+        protected void btnInsertarActualizar_Click(object sender, EventArgs e)
+        {
+            guardarActualizar(Convert.ToInt32(hdfId.Value));
+            btnInsertarActualizar.Text = "Guardar";
+            txtPrecio.Text = "";
+            //txtNombreProducto.Text = "";
+            txtCodNumerico.Text = "";
+            txtCantidad.Text = "";
+            CargarTablaProductos();
+        }
+        public int guardarActualizar(int id)
+        {
+            PRO_PRODUCTO temp = new PRO_PRODUCTO();
+            int existe = 0;
+            temp.IdProducto = id;
+            temp.CodigoNumerico = Convert.ToInt32(txtCodNumerico.Text);
+            temp.IdCategoria = Convert.ToInt32(ddlCategoria.SelectedValue);
+            temp.Inventario = Convert.ToInt32(txtCantidad.Text);
+            temp.NombreProducto = txtCodNumerico.Text;
+            temp.Precio = Convert.ToInt32(txtPrecio.Text);
+
+            existe = objProd.ConsultarPorId(temp).Count();
+
+            CQR_CODIGO_QR tempQR = new CQR_CODIGO_QR();
+            
+            if (existe > 0)
+            {
+                //tempQR.idProducto = temp.IdProducto;
+                //tempQR.idQR = Convert.ToInt32(temp.IdProducto+""+ temp.CodigoNumerico);
+                //tempQR.urlImagen = generarQR(Convert.ToString(tempQR.idQR));
+                objProd.Actualizar(temp);
+                //objQR.Actualizar(tempQR);
+            }
+            else
+            {
+                if (objProd.Existe(temp))
+                {
+                    ErrorMensajer("Producto ya existe con esta categoria!");
+                }else
+                {
+                    temp = objProd.Insertar(temp);
+                }
+                
+
+                //tempQR.idProducto = temp.IdProducto;
+                //tempQR.idQR = Convert.ToInt32(temp.IdProducto + "" + temp.CodigoNumerico);
+                //tempQR.urlImagen = generarQR(Convert.ToString(tempQR.idQR));
+
+                //objQR.Insertar(tempQR);
+            }
+            return 1;
+        }
+
+        public void CargarTablaProductos()
+        {
+            try
+            {
+                var dt = new DataTable();
+                var rows = objProd.Consultar();
+
+                dt.Columns.Add("IdProducto", typeof(System.String));
+                dt.Columns.Add("NombreProducto", typeof(System.String));
+                dt.Columns.Add("IdCategoria", typeof(System.String));
+                dt.Columns.Add("CodigoNumerico", typeof(System.String));
+                dt.Columns.Add("Precio", typeof(System.String));
+                dt.Columns.Add("Inventario", typeof(System.String));
+
+                foreach (PRO_PRODUCTO r in rows)
+                {
+                    DataRow fila = dt.NewRow();
+
+                    fila["IdProducto"] = r.IdProducto;
+                    fila["NombreProducto"] = r.NombreProducto;
+                    fila["IdCategoria"] = r.IdCategoria;
+                    fila["CodigoNumerico"] = r.CodigoNumerico;
+                    fila["Precio"] = r.Precio;
+                    fila["Inventario"] = r.Inventario;
+                    dt.Rows.Add(fila);
+                }
+                gvwProductos.DataSource = dt;
+                gvwProductos.DataBind();
+            }
+            catch (Exception ex)
+            {
+                var err = ex.Message;
+            }
+        }
+        protected void gvwProductos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            hdfId.Value = gvwProductos.SelectedRow.Cells[0].Text;
+            //txtNombreProducto.Text = gvwProductos.SelectedRow.Cells[1].Text;
+            ddlCategoria.SelectedValue = gvwProductos.SelectedRow.Cells[1].Text;
+            txtCodNumerico.Text = gvwProductos.SelectedRow.Cells[2].Text;
+            txtPrecio.Text = gvwProductos.SelectedRow.Cells[3].Text;
+            txtCantidad.Text = gvwProductos.SelectedRow.Cells[4].Text;
+            btnInsertarActualizar.Text = "Actualizar";
+
+            //var temp = new CQR_CODIGO_QR();
+            //temp.idQR = Convert.ToInt32(hdfId.Value + "" + txtCodNumerico.Text);
+            //temp = objQR.ConsultarPorId(temp).FirstOrDefault();
+
+            //QRImage.ImageUrl = "../" + temp.urlImagen.Substring(70);
+            //QRImage.ImageUrl = "../" + temp.urlImagen.Substring(32);
+        }
+
+        protected void txtCodNumerico_TextChanged(object sender, EventArgs e)
+        {
+            txtPrecio.Text = txtCodNumerico.Text + "00";
+        }
+    }
+}
