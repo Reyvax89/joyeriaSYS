7b8802b40b7f8fbb0c6c72e8526eda756c867532
mauricionm <mau18nm@gmail.com>
2018-04-23 22:43:38 -0600

    Added file.

---
+++ joyeriaSYS/Enum/EstadoFacturas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace joyeriaSYS.Enum
+{
+    public enum EstadoFacturas: int
+    {
+        EnCreacion = 0,
+        Finalizada = 1,
+        Cancelada = 2
+    }
+}
